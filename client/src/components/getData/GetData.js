3e7af83662ea60c04418939f87a15abe40474529
Niral H Lad <43843579+niralhlad@users.noreply.github.com>
2023-01-31 11:43:58 -0500

    removing unused imports

--- client/src/components/getData/GetData.js
+++ client/src/components/getData/GetData.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import './GetData.css';
-const config = require('../../config.js');
 
 //import static values
 const spellStatic = require('../../staticData/spell.json');
